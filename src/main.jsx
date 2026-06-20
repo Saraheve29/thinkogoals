afb7f56de1fdbefab34c60fa4845be24447468b8
Saraheve29 <sarahwhyte33@gmail.com>
2026-06-20 22:27:57 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import ThinkoGoalsApp from "./ThinkoGoals.jsx";
+import ThinkoGoalsApp from "./Thinkogoals.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
