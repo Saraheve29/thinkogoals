1ac108b53a55f665e7baecce2b808ff15a9d5450
Saraheve29 <sarahwhyte33@gmail.com>
2026-06-20 23:37:20 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import ThinkoGoalsApp from "./thinkogoals.jsx";
 
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js").catch(() => {});
+  });
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThinkoGoalsApp />
